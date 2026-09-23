0ce8f3aa28fe5e01a16a1e087b16c8e513f74f2c
xlarisaa45 <larisaoarcea22@gmail.com>
2026-09-23 17:50:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("surpriseButton");
+const message = document.getElementById("surpriseMessage");
+
+button.addEventListener("click", function() {
+
+    message.textContent =
+        "Ești una dintre cele mai importante persoane pentru mine! 💗 La mulți ani! 🎂✨";
+
+});
+
+const parolaCorecta = "1234";
